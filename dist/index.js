15a944570897bfb76731cd92bd3ada91b40cec02
Bren Norris <bnorris@enterrasolutions.com>
2021-06-09 16:44:58 -0400

    added application insights

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AzureDataTablesClient = void 0;
+let appInsights = require('applicationinsights');
 var client_1 = require("./client");
 Object.defineProperty(exports, "AzureDataTablesClient", { enumerable: true, get: function () { return client_1.AzureDataTablesClient; } });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
+let appInsights = require('applicationinsights');
 export { AzureDataTablesClient } from './client';
