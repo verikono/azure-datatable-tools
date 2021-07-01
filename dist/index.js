7301026660a06fd2d8f38c49450f1380c4247bac
Bren Norris <bnorris@enterrasolutions.com>
2021-07-01 11:44:46 -0400

    removed a fragment of application insights

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AzureDataTablesClient = void 0;
-let appInsights = require('applicationinsights');
 var client_1 = require("./client");
 Object.defineProperty(exports, "AzureDataTablesClient", { enumerable: true, get: function () { return client_1.AzureDataTablesClient; } });
 //# sourceMappingURL=index.js.map
